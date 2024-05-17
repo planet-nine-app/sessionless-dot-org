42446086d17598612eb07b1d117443797c38fd23
omaneh <122321976+omaneh@users.noreply.github.com>
2024-05-16 20:47:32 -0400

    Switched the layout

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const sparklesBlack = document.getElementById('black-sparkles');
 const btn2 = document.getElementById('btn2');
 const wOpenBook = document.getElementById('w-open-book');
 const bOpenBook = document.getElementById('b-open-book');
+const btn3 = document.getElementById('btn3');
+const sparklesWhite2 = document.getElementById('white-sparkles2');
+const sparklesBlack2 = document.getElementById('black-sparkles2');
 
 btn1.addEventListener('mouseover', () => {
   sparklesWhite.classList.add('visible');
@@ -18,6 +21,20 @@ btn1.addEventListener('mouseout', () => {
     sparklesBlack.classList.remove('hidden');
     sparklesBlack.classList.add('visible');
 });
+
+btn3.addEventListener('mouseover', () => {
+    sparklesWhite2.classList.add('visible');
+    sparklesWhite2.classList.remove('hidden');
+    sparklesBlack2.classList.add('hidden');
+    sparklesBlack2.classList.remove('visible');
+  });
+  
+  btn3.addEventListener('mouseout', () => {
+      sparklesWhite2.classList.remove('visible');
+      sparklesWhite2.classList.add('hidden');
+      sparklesBlack2.classList.remove('hidden');
+      sparklesBlack2.classList.add('visible');
+  });
 
 btn2.addEventListener('mouseover', () => {
     wOpenBook.classList.add('visible');
