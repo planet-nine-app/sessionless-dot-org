6e1ec7dd922a6a15134b216714fb6011e9c4864d
omaneh <122321976+omaneh@users.noreply.github.com>
2024-05-23 18:14:44 -0400

    Added popup with no styling

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const bOpenBook = document.getElementById('b-open-book');
 const btn3 = document.getElementById('btn3');
 const sparklesWhite2 = document.getElementById('white-sparkles2');
 const sparklesBlack2 = document.getElementById('black-sparkles2');
-
 const btn4 = document.getElementById('btn4');
 const whiteHeart = document.getElementById('white-hearts');
 const blackHeart = document.getElementById('black-hearts');
+const popupBg = document.getElementById("popup-bg");
+const popup = document.getElementById("popup")
+const btnClosePopup = document.getElementById("close-popup")
 
 btn1.addEventListener('mouseover', () => {
   sparklesWhite.classList.add('visible');
@@ -66,4 +68,18 @@ btn2.addEventListener('mouseover', () => {
       whiteHeart.classList.add('hidden');
       blackHeart.classList.remove('hidden');
       blackHeart.classList.add('visible');
+  });
+
+  btn4.addEventListener('click', () => {
+    popupBg.classList.add('visible');
+    popupBg.classList.remove('hidden');
+    popup.classList.add('visible');
+    popup.classList.remove('hidden');
+  });
+
+  btnClosePopup.addEventListener('click', () => {
+    popupBg.classList.remove('visible');
+    popupBg.classList.add('hidden');
+    popup.classList.remove('visible');
+    popup.classList.add('hidden');
   });
